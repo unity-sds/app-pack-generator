379370666cbb04b327bf9b87da3701a4fb80b47e
Max Zhan <max.zhan@jpl.nasa.gov>
2022-05-17 17:58:19 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,17 +8,12 @@ ActivateVEnv="source $ParserDir/env/bin/activate"
 TestingDir="/home/zhan/downsample-landsat"
 ARTIFACT_DIR="$WorkingDir/artifact-deposit-repo"
 ARTIFACT_URL="git@github.com:jplzhan/artifact-deposit-repo.git"
+GIT_SSH_COMMAND="ssh -i $ARTIFACT_SSH"
 
 echo "$(ls -la $ParserDir)"
 
-whoami
-
-ls -la $ARTIFACT_SSH
-
-GIT_SSH_COMMAND="ssh -i $ARTIFACT_SSH" git clone "$ARTIFACT_URL" "$ARTIFACT_DIR"
+git clone "$ARTIFACT_URL" "$ARTIFACT_DIR"
 
 $ActivateVEnv
-#env ARTIFACT_DIR=$ARTIFACT_DIR $PythonExe "$Parser" "$repository" "$checkout"
+env ARTIFACT_DIR=$ARTIFACT_DIR $PythonExe "$Parser" "$repository" "$checkout"
 deactivate
-
-ls -la
