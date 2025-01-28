c80423300bf49c5a877c8c31ae28e061ff840742
James McDuffie <James.McDuffie@jpl.nasa.gov>
2025-01-28 12:59:14 -0800

    Reorganize CWL creation so that process.cwl can be generated seperately from the other CWL files in the monolthic application package approach. Rename CWL variables to match the modular approach where appropriate.

--- app_pack_generator/__init__.py
+++ app_pack_generator/__init__.py
@@ -1,6 +1,6 @@
 from .git import GitManager, GitRepoError
 from .docker import DockerUtil
 from .application import ApplicationNotebook
-from .process import CWL, Descriptor
+from .process import ProcessCWL, BundledDataStagingCWL, Descriptor
 
 from .version import __version__
